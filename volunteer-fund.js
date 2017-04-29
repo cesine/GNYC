60cbfb686ce3f8fce089be83013c50e3b2ef33d8
Alexandra Udinov <rosanique.l.hall@student.uncommonhs.org>
2017-04-29 19:19:04 +0000

    finised text

---
+++ volunteer-fund.js
@@ -0,0 +1,24 @@
+$("#volunteer").submit(function(e) {
+    e.preventDefault();
+    alert ("Welcome!");    
+
+});
+
+
+$("#fund").submit(function(e) {
+    e.preventDefault();
+    
+    // get all the inputs into an array.
+    var $inputs = $('#fund :input');
+
+    // not sure if you wanted this, but I thought I'd add it.
+    // get an associative array of just the values.
+    var values = {};
+    $inputs.each(function()  {
+        values[this.name] = $(this).val();
+    });
+    
+    console.log ("here are the values",values);
+    alert (" Thank You for your donation of " + values.amount + "!");    
+
+});
